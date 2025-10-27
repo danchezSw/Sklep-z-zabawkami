e9603b9f6616dd8b5ba869ff5956850ac2d26168
mat11123 <mateuszpindych@wp.pl>
2025-10-27 11:00:17 +0100

    Dodano folder z projektem

---
+++ ToyStore/ToyStore/Controllers/ShopController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToyStore.Web.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            // w przyszłości można dodać pobieranie szczegółów produktu po ID
+            return View();
+        }
+    }
+}

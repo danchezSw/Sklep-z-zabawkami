5610a44bd5196802c43f73d13dc235c2602f7603
wiktorstelmach8@gmail.com <wiktorstelmach8@gmail.com>
2025-11-15 00:50:06 +0100

    katalog ogarnięty i widok dla koszyka

---
+++ ToyStore/ToyStore/Controllers/CartController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToyStore.Web.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ToyStore.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Pobierz koszyk z LocalStorage lub sesji — tutaj dla przykładu pusta lista
+            var cartItems = new List<CartItem>();
+
+            // Przykładowe dane jeśli chcesz testować
+            // cartItems.Add(new CartItem { ProductName = "LEGO Hogwarts", Quantity = 1, Price = 299.99M, Image = "~/images/lego_hogwarts_castle.jpg" });
+
+            return View(cartItems);
+        }
+    }
+}
---
+++ ToyStore/ToyStore/Models/CartItem.cs
@@ -0,0 +1,13 @@
+﻿namespace ToyStore.Web.Models
+{
+    public class CartItem
+    {
+        public string ProductKey { get; set; }
+        public string ProductName { get; set; } = "";
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; } = "";
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
